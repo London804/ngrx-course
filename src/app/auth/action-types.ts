b834ca7a0371247dbca1d837927bee83c0a100ad
Alex Chaparro <achaparro@zipari.com>
2019-11-20 12:32:21 -0500

    completed key concepts summary - NgRx Actions and Reducers In Action

---
+++ src/app/auth/action-types.ts
@@ -0,0 +1,8 @@
+// this file is used to group all our auth actions together
+// we are exporting it as AuthActions so we can use it throughout the application that way.
+import * as AuthActions from './auth.actions';
+// the * allows you import everything
+// the as AuthActions is an alias to everything
+
+
+export { AuthActions };
